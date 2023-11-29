8adae9e6c031d3cde74adf5257651a00a8adf4c9
Gary Kwong [:gkw] <nth10sd@gmail.com>
2023-11-29 06:47:55 -0800

    Add a naive Python truncate_file_lines function

--- python/rustpy01/truncate.py
+++ python/rustpy01/truncate.py
@@ -14,6 +14,27 @@ if TYPE_CHECKING:
     from collections.abc import Iterable
 
 
+def py_truncate_file_lines(filename: Path | str, lines_wanted: int) -> None:
+    """Truncate files to desired number of lines, similar algorithm to naive Rust one.
+
+    :param filename: Filename of file to truncate
+    :param lines_wanted: Desired number of lines to retain
+    """
+    temp_file = Path(filename).with_suffix("").with_suffix(f"{Path(filename).suffix}~")
+    with Path(filename).open(encoding="utf-8") as f, temp_file.open(
+        "w", encoding="utf-8"
+    ) as g:
+        for idx, line in enumerate(f.readlines()):
+            if idx >= lines_wanted:
+                break
+            g.write(line)
+
+    target_file = temp_file.with_suffix("").with_suffix(
+        f'{Path(filename).suffix.removesuffix("~")}'
+    )
+    temp_file.replace(target_file)
+
+
 def fast_py_truncate_file_lines(filename: Path | str, lines_wanted: int) -> None:
     """Truncate files to desired number of lines, optimized algorithm in Python.
 
@@ -35,8 +56,8 @@ def sample_truncation() -> None:
     desired_file = root_folder / "enwik9.txt"
 
     if desired_file.is_file():
-        # Swap this with fast_py_truncate_file_lines or vice versa
-        truncate_file_lines(desired_file, 1000000)
+        # Swap this with py_truncate_file_lines or vice versa
+        truncate_file_lines(desired_file, 100000)
 
 
 sample_truncation()
--- python/tests/test_truncate.py
+++ python/tests/test_truncate.py
@@ -10,9 +10,25 @@ from typing import TYPE_CHECKING
 # pylint: disable=no-name-in-module
 from rustpy01._rustpy01 import truncate_file_lines
 from rustpy01.truncate import fast_py_truncate_file_lines
+from rustpy01.truncate import py_truncate_file_lines
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+
+def test_py_truncate_file_lines(tmp_path: Path) -> None:
+    """Test the py_truncate_file_lines function.
+
+    :param tmp_path: Fixture from pytest for creating a temporary directory
+    """
+    temp_file = tmp_path / "a.txt"
+    temp_file.write_text("123\n456\n78", encoding="utf-8")
+    py_truncate_file_lines(temp_file, 2)
+
+    expected_file = tmp_path / "expected.txt"
+    expected_file.write_text("123\n456\n", encoding="utf-8")
+
+    assert cmp(temp_file, expected_file)
 
 
 def test_fast_py_truncate_file_lines(tmp_path: Path) -> None:
